18627191faaac529ccfec4dfee9e2af8de7c927b
Ludwig <00ludwighahn@gmail.com>
2025-03-05 20:28:47 +0100

    Renamed Main.java to ShapeApp.java

--- src/main/java/org/example/ShapeApp.java
+++ src/main/java/org/example/ShapeApp.java
@@ -1,13 +1,13 @@
 package org.example;
 
-public class Main {
+public class ShapeApp {
     public static void main(String[] args) {
         Shape circle = new Circle("Green", 5.0);
         circle.draw();
-        System.out.println(circle.toString() + "\n");
+        System.out.println(circle + "\n");
 
         Shape rectangle = new Rectangle("Blue", 3.0, 5.0);
         rectangle.draw();
-        System.out.println(rectangle.toString());
+        System.out.println(rectangle);
     }
 }
