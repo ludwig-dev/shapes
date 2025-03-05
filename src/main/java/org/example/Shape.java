4a6157dfa72a89c0f9d678d426736f646ff9688b
Ludwig <00ludwighahn@gmail.com>
2025-03-05 19:57:26 +0100

    Added Drawable interface and abstract class for shapes with area method

---
+++ src/main/java/org/example/Shape.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public abstract class Shape {
+    protected String color;
+
+    public Shape(String color){
+        this.color = color;
+    }
+
+    public abstract double area();
+
+    @Override
+    public String toString(){
+        return "Color: " + this.color + ", Area: " + area();
+    }
+}
