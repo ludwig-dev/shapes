acfa8780de4f48f795e9a92790cfdb2f7eb7e90e
Ludwig <00ludwighahn@gmail.com>
2025-03-05 20:24:42 +0100

    Created Rectangle.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Shape circle = new Circle("Green", 5.0);
         circle.draw();
-        System.out.println(circle.area());
+        System.out.println(circle.toString() + "\n");
+
+        Shape rectangle = new Rectangle("Blue", 3.0, 5.0);
+        rectangle.draw();
+        System.out.println(rectangle.toString());
     }
 }
---
+++ src/main/java/org/example/Rectangle.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a " + color + " Rectangle of size " + width + "x" + height);
+    }
+}
