72809be011ba30c6a64ff3dcebd18ecc877877cf
Ludwig <00ludwighahn@gmail.com>
2025-03-05 20:44:04 +0100

    added test for rectangle area

--- src/test/java/ShapeTest.java
+++ src/test/java/ShapeTest.java
@@ -1,16 +1,21 @@
 
 import org.example.Circle;
+import org.example.Rectangle;
 import org.example.Shape;
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShapeTest {
 
     @org.junit.Test
-    @Test
     public void testCircleArea(){
         Shape circle = new Circle("Red", 5.0);
         assertEquals(78.54, circle.area(), 0.01);
+    }
+
+    @org.junit.Test
+    public void testRectangleArea() {
+        Shape rectangle = new Rectangle("Red", 3.0,6.0);
+        assertEquals(18.0, rectangle.area());
     }
 }
